53e92b92d59a8f67a33493406d3a99c3eebb9bba
spy-1234 <janmejaisinghvasu20@gmail.com>
2025-05-15 14:12:45 +0530

    adding-and-initialize

---
+++ controllers/add.js
@@ -0,0 +1,5 @@
+async function addRepo() {
+    console.log("Adding file to the staging area...");
+}
+
+module.exports = { addRepo }
---
+++ controllers/init.js
@@ -0,0 +1,5 @@
+async function initRepo(){
+    console.log("Initializing repository...");
+}
+
+module.exports = { initRepo }
