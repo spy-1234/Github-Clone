ab469835f2f44691b06250b3c80f969323cf5d7a
spy-1234 <janmejaisinghvasu20@gmail.com>
2025-05-16 23:54:02 +0530

    basic-function-of-github

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const yargs = require("yargs")
 const { hideBin } = require("yargs/helpers")
+
+
 const { initRepo } = require("./controllers/init")
 const { addRepo } = require("./controllers/add")
+const { commitRepo } = require("./controllers/commit")
+const { pushRepo } = require("./controllers/push")
+const { pullRepo } = require("./controllers/pull")
+const { revertRepo } = require("./controllers/revert")
 
 
 yargs(hideBin(process.argv))
@@ -12,5 +18,23 @@ yargs(hideBin(process.argv))
         type: "string",
     })
 }, addRepo)
+.command('commit <message>', "commit the staged files", (yargs) => {
+    yargs.positional("message", {
+        describe : "commit message",
+        type: "string",
+    })
+}, commitRepo)
+.command("push", "Push the commits to S3", {}, pushRepo)
+.command("pull", "Pull the commits from S3", {}, pullRepo)
+.command(
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+        yargs.positional("commitID", {
+            describe: "commit ID to revert to",
+            type: "string",
+        }, revertRepo)
+    }
+)
 .demandCommand(1, "You need at least one command before moving on")
 .help().argv
